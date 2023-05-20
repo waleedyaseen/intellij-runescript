c8580af52fef07ff2c2513e9ca9004c95f56bc48
Walied K. Yassen <walied.developer@hotmail.com>
2023-05-20 15:37:30 +0300

    Add base refactoring support

---
+++ src/main/kotlin/io/runescript/plugin/ide/refactoring/RsRefactoringSupportProvider.kt
@@ -0,0 +1,12 @@
+package io.runescript.plugin.ide.refactoring
+
+import com.intellij.lang.refactoring.RefactoringSupportProvider
+import com.intellij.psi.PsiElement
+import io.runescript.plugin.lang.psi.impl.RuneScriptLocalVariableExpressionImpl
+
+class RsRefactoringSupportProvider : RefactoringSupportProvider() {
+
+    override fun isInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
+        return element is RuneScriptLocalVariableExpressionImpl
+    }
+}
