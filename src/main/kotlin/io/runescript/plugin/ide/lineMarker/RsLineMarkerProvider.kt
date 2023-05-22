632d3c0bb3f97832842b6ece3abe05e502c6db8d
Walied K. Yassen <walied.developer@hotmail.com>
2023-05-22 19:21:44 +0300

    Fix performance warning issue with line markers

--- src/main/kotlin/io/runescript/plugin/ide/lineMarker/RsLineMarkerProvider.kt
+++ src/main/kotlin/io/runescript/plugin/ide/lineMarker/RsLineMarkerProvider.kt
@@ -5,17 +5,17 @@ import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.psi.PsiElement
 import io.runescript.plugin.lang.psi.RsScript
-import io.runescript.plugin.lang.psi.RsStringLiteralExpression
 
 class RsLineMarkerProvider : LineMarkerProvider {
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
         if (element is RsScript) {
             // TODO(Walied): Find a better way around icons and text
+            val targetElement = element.scriptHeader.scriptName.nameExpression!!.identifier!!
             val presentation = element.presentation!!
             return NavigationGutterIconBuilder.create(presentation.getIcon(false)!!)
-                    .setTarget(element)
+                    .setTarget(targetElement)
                     .setTooltipText("Navigate to ${presentation.presentableText}")
-                    .createLineMarkerInfo(element)
+                    .createLineMarkerInfo(targetElement)
         }
         return null
     }
