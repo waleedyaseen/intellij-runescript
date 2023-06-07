e52e5c374aa18c7e2605572e95b22a7b8ee048d2
Walied K. Yassen <walied.developer@hotmail.com>
2023-06-08 01:10:51 +0300

    Implement smart inlay hints hiding

--- src/main/kotlin/io/runescript/plugin/ide/codeInsight/RsInlayParameterHintsProvider.kt
+++ src/main/kotlin/io/runescript/plugin/ide/codeInsight/RsInlayParameterHintsProvider.kt
@@ -4,9 +4,19 @@ import com.intellij.codeInsight.hints.InlayInfo
 import com.intellij.codeInsight.hints.InlayParameterHintsProvider
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.suggested.startOffset
+import io.runescript.plugin.lang.psi.RsArrayAccessExpression
+import io.runescript.plugin.lang.psi.RsBooleanLiteralExpression
 import io.runescript.plugin.lang.psi.RsCommandExpression
+import io.runescript.plugin.lang.psi.RsConstantExpression
+import io.runescript.plugin.lang.psi.RsExpression
 import io.runescript.plugin.lang.psi.RsGosubExpression
+import io.runescript.plugin.lang.psi.RsIntegerLiteralExpression
+import io.runescript.plugin.lang.psi.RsLocalVariableExpression
+import io.runescript.plugin.lang.psi.RsNullLiteralExpression
+import io.runescript.plugin.lang.psi.RsParExpression
+import io.runescript.plugin.lang.psi.RsScopedVariableExpression
 import io.runescript.plugin.lang.psi.RsScriptHeader
+import io.runescript.plugin.lang.psi.RsStringLiteralExpression
 import io.runescript.plugin.lang.psi.op.RsOpCommand
 
 @Suppress("UnstableApiUsage")
@@ -24,11 +34,52 @@ class RsInlayParameterHintsProvider : InlayParameterHintsProvider {
                 break
             }
             val parameter = parametersList[parameterIndex]
-            inlayHints += InlayInfo(parameter, argument.startOffset)
+            if (!shouldHideHint(parameter, argument)) {
+                inlayHints += InlayInfo(parameter, argument.startOffset,
+                        isShowOnlyIfExistedBefore = false,
+                        isFilterByBlacklist = false,
+                        relatesToPrecedingText = false
+                )
+            }
             parameterIndex += guesstimateReturnCount(argument)
         }
         return inlayHints
     }
+
+    private fun shouldHideHint(parameterName: String, argument: RsExpression): Boolean {
+        val argumentName = when (argument) {
+            is RsConstantExpression -> argument.nameLiteral.text
+            is RsGosubExpression -> argument.nameLiteral.text
+            is RsCommandExpression -> argument.nameLiteral.text
+            is RsLocalVariableExpression -> argument.nameLiteral.text
+            is RsScopedVariableExpression -> argument.nameLiteral.text
+            is RsArrayAccessExpression -> (argument.expressionList[0] as RsLocalVariableExpression).nameLiteral.text
+            else -> null
+        }
+        if (parameterName.length < 3) {
+            return true
+        }
+        if (argumentName != null) {
+            if (argumentName.length < 3) {
+                return true
+            }
+            val argName = argumentName.lowercase()
+            val paramName = parameterName.lowercase()
+            if (paramName.contains(argName) || argName.contains(paramName)) {
+                return true
+            }
+        }
+        var unwrappedExpression = argument
+        while (unwrappedExpression is RsParExpression) {
+            unwrappedExpression = unwrappedExpression.expression
+        }
+        return unwrappedExpression !is RsIntegerLiteralExpression
+                && unwrappedExpression !is RsBooleanLiteralExpression
+                && unwrappedExpression !is RsStringLiteralExpression
+                && unwrappedExpression !is RsNullLiteralExpression
+    }
+
+
 
     private fun getArgumentsList(element: PsiElement) = when (element) {
         is RsGosubExpression -> element.argumentList?.expressionList
