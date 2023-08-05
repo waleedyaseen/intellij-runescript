b9a15f36d08c1f4f2ecf258adec8748fd657047b
Walied K. Yassen <walied.developer@hotmail.com>
2023-08-05 18:04:27 +0300

    Improve the project template generator

--- src/main/kotlin/io/runescript/plugin/ide/projectWizard/RsProjectTemplate.kt
+++ src/main/kotlin/io/runescript/plugin/ide/projectWizard/RsProjectTemplate.kt
@@ -3,24 +3,56 @@ package io.runescript.plugin.ide.projectWizard
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.findOrCreateDirectory
 
 class RsProjectTemplate {
 
     fun generateTemplate(module: Module, contentEntry: ContentEntry, projectDir: VirtualFile) {
-        val gitIgnoreFile = projectDir.createChildData(this, ".gitignore")
-        val srcDir = projectDir.createChildDirectory(projectDir, "src")
+        createGitIgnoreFile(projectDir)
+        createEditorConfigFile(projectDir)
+        val srcDir = projectDir.findChild("src") ?: projectDir.createChildDirectory(projectDir, "src")
         contentEntry.addSourceFolder(srcDir, false)
-        val packDir = projectDir.createChildDirectory(projectDir, "pack")
+        val packDir = projectDir.findChild("pack") ?: projectDir.createChildDirectory(projectDir, "pack")
         contentEntry.addExcludeFolder(packDir)
-        projectDir.createChildDirectory(projectDir, "sym")
-        gitIgnoreFile.setBinaryContent(
+        projectDir.findOrCreateDirectory("symbols")
+    }
+
+    private fun createGitIgnoreFile(projectDir: VirtualFile) {
+        var gitIgnore = projectDir.findChild(".gitignore")
+        if (gitIgnore != null) return
+        gitIgnore = projectDir.createChildData(projectDir, ".gitignore")
+        gitIgnore.setBinaryContent(
             """
             # IntelliJ project files
             .idea/
             *.iml
+            out/
             
             # RuneScript project files
             pack/
+        """.trimIndent().toByteArray()
+        )
+    }
+
+    private fun createEditorConfigFile(projectDir: VirtualFile) {
+        var gitIgnore = projectDir.findChild(".editorconfig")
+        if (gitIgnore != null) return
+        gitIgnore = projectDir.createChildData(projectDir, ".editorconfig")
+        gitIgnore.setBinaryContent(
+            """
+            root = true
+
+            [*]
+            charset = utf-8
+            insert_final_newline = true
+            trim_trailing_whitespace = true
+            max_line_length = 120
+            indent_style = space
+            indent_size = 4
+
+            [symbols/*.sym]
+            indent_style = tab
+            trim_trailing_whitespace = false
         """.trimIndent().toByteArray()
         )
     }
