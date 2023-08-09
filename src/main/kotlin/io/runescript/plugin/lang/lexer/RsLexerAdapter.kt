2fd6e85d9ea211c4e42923d8513411d7e6b23629
Walied K. Yassen <walied.developer@hotmail.com>
2023-08-09 16:12:20 +0300

    Merge sequential STRING_PART tokens

--- src/main/kotlin/io/runescript/plugin/lang/lexer/RsLexerAdapter.kt
+++ src/main/kotlin/io/runescript/plugin/lang/lexer/RsLexerAdapter.kt
@@ -1,5 +1,10 @@
 package io.runescript.plugin.lang.lexer
 
 import com.intellij.lexer.FlexAdapter
+import com.intellij.lexer.MergingLexerAdapter
+import com.intellij.psi.tree.TokenSet
+import io.runescript.plugin.lang.psi.RsElementTypes
 
-class RsLexerAdapter(lexerInfo: RsLexerInfo) : FlexAdapter(_RsLexer(null, lexerInfo))
+class RsLexerAdapter(lexerInfo: RsLexerInfo) : MergingLexerAdapter(
+    FlexAdapter(_RsLexer(null, lexerInfo)), TokenSet.create(RsElementTypes.STRING_PART)
+)
